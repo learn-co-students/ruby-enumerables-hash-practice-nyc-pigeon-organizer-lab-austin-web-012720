11ce690bd544dc66ad05b041154b58b31348f281
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-10 01:37:39 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,50 @@
+require 'pry'
+
+def set_name_keys (data)
+  new_hash = {}
+  data.each do |first_keys, data_hash|
+    data_hash.each do |attributes, array|
+        array.each do |name|
+          if !new_hash.has_key?(name)
+            new_hash[name] = {}
+          end
+        end
+      end
+    end
+new_hash
+end
+
+def set_attributes_keys(data, new_hash)
+  data.each do |first_keys, data_hash|
+        data_hash.each do |attributes, array|
+                      array.each do |name|
+                                if !new_hash.has_key?(attributes)
+                                  new_hash[name][first_keys] = []
+                                end
+                                end
+                      end
+            end
+new_hash
+end
+
+
+def set_values(data, new_hash)
+  data.each do |first_keys, data_hash|
+    data_hash.each do |attributes, array|
+      array.each do |name|
+        if !array.include?(new_hash[name])
+          new_hash[name][first_keys] << "#{attributes}"
+        end
+                 end
+                   end
+            end
+new_hash
+end
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  named_hash = set_name_keys(data)
+  colored_hash = set_attributes_keys(data, named_hash)
+  final_hash = set_values(data, colored_hash)
+  final_hash
 end
