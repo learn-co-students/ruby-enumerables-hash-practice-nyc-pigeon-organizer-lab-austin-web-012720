ccd7f5fe92aacb37bd4f358eef8085479df1bd79
Phoenix R <phoenixrabang@gmail.com>
2020-01-19 23:55:00 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  pigeon_list = {}
+  
+  data.each do |first_order_keys, value|
+    value.each do |second_order_keys, array|
+      array.each do |name|
+        if pigeon_list [name] == nil
+          pigeon_list [name] = {}
+        end
+      end
+      
+      if pigeon_list[name][first_order_keys] == nil
+        pigeon_list[name][first_order_keys] = []
+      end
+      
+      pigeon_list[name][first_order_keys].push(second_order_keys.to_s)
+    end
